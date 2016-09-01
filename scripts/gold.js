406de9f3d536b78bc354e8a353b8ee34a82e8b61
Corey Hobbs <coreyhobbs@github.com>
2016-08-31 20:56:35 -0700

    cleanup house

--- scripts/gold.js
+++ scripts/gold.js
@@ -1,20 +1,7 @@
 var Client = require('node-rest-client').Client;
 var testAuth = require("./../testAuth.json");
 
-
-// var octo = new Octokat({
-//   token: process.env.G_TOKEN
-// });
-
 var client = new Client();
-
-// direct way
-// client.get("http://api.github.com/gists/public", function (data, response) {
-//     // parsed response body as js object
-//     console.log(data);
-//     // raw response
-//     console.log(response);
-// });
 
 // set content-type header and data as json in args parameter
 var args = {
