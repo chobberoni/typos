56759e1a0e4fe1ea2ba1a6929fa00961817cf68b
Corey Hobbs <coreyhobbs@github.com>
2016-09-20 21:06:09 -0700

    EOL this freaking project 😞

--- scripts/server/gold.js
+++ scripts/server/gold.js
@@ -1,5 +1,5 @@
 var Client = require('node-rest-client').Client;
-var testAuth = require("./../testAuth.json");
+var testAuth = require("./../../testAuth.json");
 
 var client = new Client();
 
@@ -11,20 +11,44 @@ var args = {
 // registering remote methods
 client.registerMethod("gistsMethod", "https://api.github.com/gists/public", "GET");
 client.registerMethod("bigSearchMethod", "https://api.github.com/search/code?q=search+in:file+language:js+user:jquery", "GET");
+client.registerMethod("bigSearchMethod", "https://api.github.com/search/code?q=search+in:file+language:js+user:jquery", "GET");
 
-module.exports = {
-  //calling some f(x)s
-  client.methods.gistsMethod(args, function (data, response) {
-    // parsed response body as js object
-    console.log(data);
-    // raw response
-    //console.log(response);
-  });
+// var searchCalls = function() {};
+//
+// searchCalls.prototype.bigsearch = function(){
+//   client.get("https://api.github.com/search/code?q=search+in:file+language:js+user:jquery", args,
+//       function (data, response) {
+//           // parsed response body as js object
+//           console.log(data);
+//           // raw response
+//           console.log(response);
+//       });
+// };
 
-  client.methods.bigSearchMethod(args, function (data, response) {
-    // parsed response body as js object
-    console.log(data);
-    // raw response
-    //console.log(response);
-  });
-};
+// thissssss SUCKKSSSSSSSSSS :( 😂
+// module.exports = searchCalls;
+
+client.get("https://api.github.com/search/code?q=search+in:file+language:js+filename:jsRegex", args,
+    function (data, response) {
+        // parsed response body as js object
+        console.log(data);
+        // raw response
+        console.log(response);
+    });
+
+
+//
+// //calling some f(x)s
+// client.methods.gistsMethod(args, function (data, response) {
+//   // parsed response body as js object
+//   console.log(data);
+//   // raw response
+//   //console.log(response);
+// });
+//
+// client.methods.bigSearchMethod(args, function (data, response) {
+//   // parsed response body as js object
+//   console.log(data);
+//   // raw response
+//   //console.log(response);
+// });
